100271e23db0b8b74af37ccae29bb28014a0ec0d
PriaTampan00 <148883661+PriaTampan00@users.noreply.github.com>
2024-10-24 15:05:48 +0700

    Update akuj.js

--- akuj.js
+++ akuj.js
@@ -1,21 +1,17 @@
-// all clear
 function clearDisplay() {
     document.getElementById('display').value = '';
   }
   
-  // delete
   function deleteLast() {
     let display = document.getElementById('display');
     display.value = display.value.slice(0, -1);
   }
   
-  // input
   function appendToDisplay(character) {
     let display = document.getElementById('display');
     display.value += character;
   }
   
-  // calc
   function calculate() {
     let display = document.getElementById('display');
     try {
@@ -25,7 +21,6 @@ function clearDisplay() {
     }
   }
   
-  // fact
   function factorial() {
     let display = document.getElementById('display');
     let number = parseInt(display.value);
@@ -42,11 +37,5 @@ function clearDisplay() {
     
     display.value = result;
   }
-
-document.querySelectorAll('input[type="number]').forEach(input =>{
-  input.oninput = () =>{
-    if(input.value.lenght > input.maxLenght) input.value = input.value.slice(0, input.maxLenght);
-  };
-});
 
 window.alert("Situs ini hanyalah sebuah uji coba dan masih dalam pengembangan")
