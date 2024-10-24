f4267816ea9bfdfdf626ea9ad4ad6ffaf8afaf39
PriaTampan00 <148883661+PriaTampan00@users.noreply.github.com>
2024-10-24 15:11:05 +0700

    Update akuj.js

--- akuj.js
+++ akuj.js
@@ -1,41 +1,41 @@
 function clearDisplay() {
-    document.getElementById('display').value = '';
+  document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+  let display = document.getElementById('display');
+  display.value = display.value.slice(0, -1);
+}
+
+function appendToDisplay(character) {
+  let display = document.getElementById('display');
+  display.value += character;
+}
+
+function calculate() {
+  let display = document.getElementById('display');
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
   }
+}
+
+function factorial() {
+  let display = document.getElementById('display');
+  let number = parseInt(display.value);
   
-  function deleteLast() {
-    let display = document.getElementById('display');
-    display.value = display.value.slice(0, -1);
-  }
-  
-  function appendToDisplay(character) {
-    let display = document.getElementById('display');
-    display.value += character;
-  }
-  
-  function calculate() {
-    let display = document.getElementById('display');
-    try {
-      display.value = eval(display.value);
-    } catch (error) {
-      display.value = 'Error';
-    }
-  }
-  
-  function factorial() {
-    let display = document.getElementById('display');
-    let number = parseInt(display.value);
-  
-    if (isNaN(number) || number < 0) {
+  if (isNaN(number) || number < 0) {
       display.value = 'Error';  // Menangani input yang tidak valid
       return;
-    }
-  
-    let result = 1;
-    for (let i = 1; i <= number; i++) {
-      result *= i;
-    }
-    
-    display.value = result;
   }
+  
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+      result *= i;
+  }
+    
+  display.value = result;
+}
 
 window.alert("Situs ini hanyalah sebuah uji coba dan masih dalam pengembangan")
